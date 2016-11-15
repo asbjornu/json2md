3b752e587b197ccd8e061af2f584e22c343f5f10
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2016-11-15 02:11:12 +0100

    Cleanup and modernization.

--- json2md.js
+++ json2md.js
@@ -3,29 +3,29 @@
 const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 
-var validate = new Promise((fulfill, reject) => {
-    if (!argv._ || !Array.isArray(argv._) || argv._.length != 1) {
-        reject("Usage: json2md.js [/file/to/convert.json]");
-    }
-
-    var file = argv._[0];
-
-    console.log("Converting:", file);
-
-    fulfill(file);
-});
-
-function read(file) {
+function validate(arguments) {
     return new Promise((fulfill, reject) => {
-        fs.open(file, 'r', (err, fd) => {
+        if (!arguments._ || !Array.isArray(arguments._) || arguments._.length != 1) {
+            reject("Usage: json2md.js [/file/to/convert.json]");
+        }
+
+        var fileName = arguments._[0];
+
+        fulfill({ fileName });
+    });
+}
+
+function read(state) {
+    return new Promise((fulfill, reject) => {
+        fs.open(state.fileName, 'r', (err, fd) => {
             if (err) {
                 if (err.code === "ENOENT") {
-                    reject("ERROR: The file does not exist.");
+                    reject(`ERROR: The file ${state.fileName} does not exist.`);
                 } else {
                     reject(err);
                 }
             } else {
-                var stream = fs.createReadStream(file, { fd: fd });
+                var stream = fs.createReadStream(state.fileName, { fd });
                 var content = '';
 
                 stream.on('data', (chunk) => {
@@ -33,19 +33,19 @@ function read(file) {
                 });
 
                 stream.on('end', () => {
-                    fulfill(content);
+                    fulfill({ fileName: state.fileName, content });
                 });
             }
         });
     });
 }
 
-function convert(content) {
+function convert(state) {
     return new Promise((fulfill, reject) => {
-        var json = JSON.parse(content);
+        var json = JSON.parse(state.content);
 
         if (!Array.isArray(json)) {
-            reject("Not an array")
+            reject(`ERROR: The file ${state.fileName} needs to be an array.`)
         }
 
         fulfill(json.length);
@@ -61,7 +61,7 @@ function error(err) {
     process.exit(1);
 }
 
-validate
+validate(argv)
     .then(read)
     .then(convert)
     .then(end)
