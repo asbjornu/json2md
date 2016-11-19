b63b0b4930e17f1b8adf350050a85e32b1f79ee6
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2016-11-20 00:01:39 +0100

    Moved convert function into the textile module.

--- json2md.js
+++ json2md.js
@@ -4,26 +4,6 @@ const textile = require('./lib/textile.js');
 const args = require('./lib/args.js');
 const file = require('./lib/file.js');
 
-function convert(state) {
-    return new Promise((fulfill, reject) => {
-        var json = JSON.parse(state.content);
-
-        if (!Array.isArray(json)) {
-            reject(`ERROR: The file ${state.fileName} needs to be an array.`)
-        }
-
-        var converters = json.map(article => textile.convert(article.body));
-
-        return Promise.all(converters).then(convertedArticles => {
-            fulfill({
-                fileName: state.fileName,
-                content: state.content,
-                articleCount: json.length
-            });
-        }).catch(code => reject('Converting from Textile failed: ' + code));
-    });
-}
-
 function end(state) {
     console.log(`Done converting ${state.articleCount} articles.`);
 }
@@ -35,6 +15,6 @@ function error(err) {
 
 args.validate()
     .then(file.read)
-    .then(convert)
+    .then(textile.convert)
     .then(end)
     .catch(error);
